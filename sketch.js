04bb76e12bcdad8a3a9f4b599023f51195313d37
damonyu <damonyu97@hotmail.com>
2022-05-02 20:23:50 +0100

    update target image src

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let isBackControllerConnected = false;
 let targetImage
 
 function preload() {
-    targetImage = loadImage('https://raw.githubusercontent.com/DamonYuXXX/ArcheryGame/main/asserts/target.png?token=GHSAT0AAAAAABQMUIDUG55H2X7YS77XSXNOYTQAPDQ');
+    targetImage = loadImage('https://raw.githubusercontent.com/DamonYuXXX/ArcheryGame/main/asserts/target.png');
 }
 
 function setup() {
@@ -20,45 +20,55 @@ function setup() {
 
 function draw() {
     background(200);
+    scale(10);
     push()
     //ground
-    translate(0, 300, 0);
-    rotateX(-20);
+    translate(0, 300, -200);
     rotateY(frameCount);
     fill(255);
-    box(windowWidth*2, 3, 20);
+    box(3000, 3, 20);
     fill('green');
-    box(windowWidth*2, 2, windowWidth*2);
+    box(3000, 2, 3000);
 
     //stand
-    translate(-100, -200, -80);
-    rotateX(20);
+    translate(-100, -200, -33);
+    rotateX(10);
     fill('#966F33');
-    box(20, 400 ,20);
+    box(20, 600 ,20);
     translate(200, 0, 0);
-    box(20, 400 ,20);
+    box(20, 600 ,20);
     translate(-100, 100, 0);
     box(200, 20 ,20);
 
-    translate(-100, -30, -70);
-    rotateX(-40);
-    box(20, 200 ,20);
+    translate(-100, -30, -80);
+    rotateX(-30);
+    box(20, 300 ,20);
     translate(200, 0, 0);
-    box(20, 200 ,20);
+    box(20, 300 ,20);
 
     //target holder
     translate(0, -70, 60);
-    rotateX(40);
+    rotateX(30);
     box(20, 20 ,30);
     translate(-200, 0, 0);
     box(20, 20 ,30);
 
     //target
-    translate(100, -160, 0);
+    translate(100, -210, 0);
     fill(255);
     texture(targetImage);
-    box(300, 300, 20);
-    
+    box(400, 400, 20);
+
+    pop();
+    drawArrow();
+}
+
+function drawArrow() {
+    push();
+    fill(255);
+    cylinder(0.5,40);
+    translate(0, 20.5, 0)
+    cone(0.5, 1);
     pop();
 }
 
