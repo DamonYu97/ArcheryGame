36bc15e6965e60180a931af6ec23ebc1b63c5fb1
damonyu <damonyu97@hotmail.com>
2022-05-06 13:42:00 +0100

    hand detection

--- sketch.js
+++ sketch.js
@@ -1,8 +1,15 @@
 var sketch;
 let isFrontControllerConnected = false;
 let isBackControllerConnected = false;
-let targetImage
+let targetImage;
 let arrow;
+let video;
+let handpose;
+let handPosition = {
+    x: 0,
+    y: 0
+};
+let distance = 0;
 
 function preload() {
     targetImage = loadImage('https://raw.githubusercontent.com/DamonYuXXX/ArcheryGame/main/asserts/target.png');
@@ -15,24 +22,54 @@ function setup() {
     sketch.position(0, 0, 'static');
     sketch.parent('sketch_parent');
     angleMode(DEGREES);
-    /*button = createButton('Pair Controllers');
+    button = createButton('Pair Controllers');
     button.position(200, 200);
-    button.mousePressed(pariConntroller);*/
+    button.mousePressed(pariConntroller);
+    video = createCapture(VIDEO);
+    video.size(width, height);
+    handpose = ml5.handpose(video, () => {
+        console.log("model loaded");
+    });
+    handpose.on("predict", results => {
+        if (results.length > 0) {
+            handPosition = {
+                x: results[0].annotations.thumb[0][0] / 640 * width,
+                y: results[0].annotations.thumb[0][1] / 480 * height
+            }
+            //console.log(handPosition);
+        }
+    })
+    video.hide();
 }
 
 function draw() {
+
+    ambientLight(255);
     background(200);
-    scale(3);
+    //scale(1);
     push()
     //ground
-    translate(0, 300, -200);
-    rotateY(frameCount);
+    translate(0, 300, -500);
+    //rotateY(90);
     fill(255);
     box(3000, 3, 20);
     fill('green');
     box(3000, 2, 3000);
+    drawTarget();
 
+    drawArrow(width/2 - handPosition.x, handPosition.y - height/2, 1000 + distance);
+    pop();
+    fill(0, 255, 0);
+    push();
+    translate(width/2 - handPosition.x, handPosition.y - height/2, 0);
+    plane(10, 10);
+    pop();
+
+}
+
+function drawTarget() {
     //stand
+    push();
     translate(-100, -200, -33);
     rotateX(10);
     fill('#966F33');
@@ -60,30 +97,21 @@ function draw() {
     fill(255);
     texture(targetImage);
     box(400, 400, 20);
-    translate(0, 0, 75);
-    drawArrow();
-    translate(10, 0, 0);
-    drawArrow();
-    translate(-20, 0, 0);
-    drawArrow();
     pop();
+
 }
 
-function drawArrow() {
+function drawArrow(x, y, z) {
     push();
-    //rotateX(-10);
+    translate(x, -300 + y, z);
+    rotateX(-3);
     rotateY(90);
     fill('red');
+    stroke('black');
+    strokeWeight(0.2);
     scale(0.8);
     model(arrow);
     pop();
-    /*
-    push();
-    fill(255);
-    cylinder(0.5,40);
-    translate(0, 20.5, 0)
-    cone(0.5, 1);
-    pop();*/
 }
 
 function pariConntroller() {
@@ -96,15 +124,23 @@ function microBitReceivedMessage(message){
     if (message == undefined || message == null) {
         return;
     }
+    message = message.trim();
     if (!isFrontControllerConnected) {
         microBitWriteString("FA");
+        if (message.length == 1 && message.charAt(0) == 'F') {
+            isFrontControllerConnected = true;
+        }
     }
     if (!isBackControllerConnected) {
         microBitWriteString("BA");
+        if (message.length == 1 && message.charAt(0) == 'B') {
+            isBackControllerConnected = true;
+        }
     }
-    message.trim();
-    if (message.length == 1 && message.charAt(0) == 'F') {
-        isFrontControllerConnected = true;
-    } 
+
+    if (message.length > 0 && message.charAt(0) == 'D') {
+        distance = Number(message.substr(1));
+        console.log(distance);
+    }
 }
   
